2f788131304f25b0f3917131e211a62c220f15d0
Phambaonam <phambaonam12132@gmail.com>
2016-10-17 10:46:47 +0700

    my first commit

---
+++ HelloWorld/Mamxanh1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<fstream>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+using namespace std;
+int main()
+{
+	printf("Hello \n");
+	return 0;
+}
---
+++ HelloWorld/mamxanh2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<fstream>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+using namespace std;
+int main()
+{
+	printf("Hello World \n");
+	printf("Cong hoa xa hoi chu nghia Viet Nam \n");
+	printf("Doc lap * Tu do * Hanh Phuc");
+	return(0);
+	
+}
