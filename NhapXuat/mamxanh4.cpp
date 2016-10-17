0aec3b66646bccb5b7db7b151e07884a7e77438d
Phambaonam <phambaonam12132@gmail.com>
2016-10-18 00:24:22 +0700

    sep xep lai code

--- NhapXuat/mamxanh4.cpp
+++ NhapXuat/mamxanh4.cpp
@@ -9,14 +9,14 @@ using namespace std;
 
 int main()
 {
-	int a,b;
-	
 	freopen("mamxanh4.inp","r",stdin);
+	freopen("mamxanh4_output.inp","w",stdout);
+	
+	int a,b;
 	scanf("%d %d",&a,&b);
 	printf("so thu nhat o file la:%d\n",a);
 	printf("so thu hai o file la: %d\n",b);
-	
-	freopen("mamxanh4_output.inp","w",stdout);
 	printf("ket qua cua 2 so dc luu vao file mamxanh4_output.inp la: %d %d\n",a,b);
+	
 	return 0;
 }
